b78595d61ba92784238771b3e9a9296aa140f342
niyolynate <98440370+niyolynate@users.noreply.github.com>
2022-03-21 16:57:23 +0300

    Adding examples of the program
    C++ Codes

--- main.go
+++ main.go
@@ -41,3 +41,19 @@ Enter any numberber: 36
 Squre of 36 is: 6
 
 */
+
+
+//Hello World Program
+//include headers; these are modules that include functions that you may use in your
+//program; we will almost always need to include the header that
+// defines cin and cout; the header is called iostream
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // print output to user
+  cout << "Hello World!" << endl;
+  return 0;
+}
